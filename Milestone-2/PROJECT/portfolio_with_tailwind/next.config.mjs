b0595cc697288626180c9ff287957f42b6c65cbc
Ayesha <168227324+Ayesh167@users.noreply.github.com>
2025-01-24 13:14:52 +0500

    Delete Milestone-2/PROJECT directory

--- Milestone-2/PROJECT/portfolio_with_tailwind/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['i.pinimg.com','m.media-amazon.com','cdn.prod.website-files.com','assets.weforum.org','media.licdn.com'],
-    },
-};
-
-export default nextConfig;
